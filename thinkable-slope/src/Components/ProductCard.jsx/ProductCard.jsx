0b138f39014ebcad71a17171e0f503bc324f38d5
Shantanu <shantanusut2000@gmail.com>
2023-02-22 18:04:51 +0000

    day-2

--- thinkable-slope/src/Components/ProductCard.jsx/ProductCard.jsx
+++ thinkable-slope/src/Components/ProductCard.jsx/ProductCard.jsx
@@ -1,26 +1,27 @@
-import BasicFn from "../Stars"
-import styles from "./ProductCard.module.css"
+import BasicFn from "../Stars";
+import styles from "./ProductCard.module.css";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 
-const ProductCard=({Image,name,Price,Rating,id})=>{
-return(
-    <>
-    <div>   
-     <p>{id}</p>
-    <img  src={Image} alt="" />
-    <div className={styles.limited_Edition_div}>
-        <p>Limited Edition</p>
-        <img  className={styles.cart_image}  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxi_iyZY7EC6BEEFo5YZtP0rdOoxp4PRtqhQ&usqp=CAU" alt="" />
-    </div>
-     <p className={styles.productCard_name}>{name}</p>
-     <div className={styles.ProductCard_price_maindiv}>
-     <p className={styles.productCard_price}>$ {Price} <span>Comp. Value: $59.95</span> </p>
-     </div>
-     <BasicFn Rating={Rating}/>
-    </div>
-     
-    </>
-)
+const ProductCard = ({ Image, name, Price, Rating, id }) => {
+    return (
+        <>
+            <div>
+                <p>{id}</p>
+                <img src={Image} alt="" />
+                <div className={styles.limited_Edition_div}>
+                    <p>Limited Edition</p>
+                    <ShoppingCartOutlinedIcon className={styles.cart_image} />
+                </div>
+                <p className={styles.productCard_name}>{name}</p>
+                <div className={styles.ProductCard_price_maindiv}>
+                    <p className={styles.productCard_price}>
+                        $ {Price} <span>Comp. Value: $59.95</span>{" "}
+                    </p>
+                </div>
+                <BasicFn Rating={Rating} />
+            </div>
+        </>
+    );
+};
 
-}
-
-export default ProductCard
+export default ProductCard;
