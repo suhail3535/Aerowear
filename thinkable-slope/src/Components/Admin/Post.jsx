0e0de740878edf37093fdb3ee569bcf8e937186b
Shantanu <shantanusut2000@gmail.com>
2023-02-22 12:55:33 +0530

    Git Push

--- thinkable-slope/src/Components/Admin/Post.jsx
+++ thinkable-slope/src/Components/Admin/Post.jsx
@@ -3,25 +3,24 @@ import styles from "./admin.module.css";
 import { Input, Button, Stack, Box } from "@chakra-ui/react";
 
 const Post = () => {
-  return (
-    <div id={styles.container}>
-     
-      <Box id={styles.first}> </Box>
-      <Box id={styles.second}>
-        <form>
-          <Stack spacing={3}>
-            <Input type="url" placeholder="image" size="md" />
-            <Input type="text" placeholder="name" size="md" />
-            <Input type="number" placeholder="price" size="md" />
-            <Input type="text" placeholder="category" size="md" />
-          </Stack>
-          <Button id={styles.btn} colorScheme="blue">
-            Button
-          </Button>
-        </form>
-      </Box>
-    </div>
-  );
+    return (
+        <div id={styles.container}>
+            <Box id={styles.first}> </Box>
+            <Box id={styles.second}>
+                <form>
+                    <Stack spacing={3}>
+                        <Input type="url" placeholder="image" size="md" />
+                        <Input type="text" placeholder="name" size="md" />
+                        <Input type="number" placeholder="price" size="md" />
+                        <Input type="text" placeholder="category" size="md" />
+                    </Stack>
+                    <Button id={styles.btn} colorScheme="blue">
+                        Button
+                    </Button>
+                </form>
+            </Box>
+        </div>
+    );
 };
 
 export default Post;
--- thinkable-slope/src/Pages/MainRoutes.jsx
+++ thinkable-slope/src/Pages/MainRoutes.jsx
@@ -1,33 +1,21 @@
-
+import { Routes, Route } from "react-router-dom";
+import Clearance from "./Clearance/Clearance";
+import Men from "./Men/Men";
+import Women from "./Women/Women";
 import React from "react";
 import Navbar from "../Components/Navbar/Navbar";
 
 const MainRoutes = () => {
     return (
         <div>
-            <Navbar />
+            {/* <Navbar /> */}
+            <Routes>
+                <Route path="/women" element={<Women />} />
+                <Route path="/men" element={<Men />} />
+                <Route path="/clearance" element={<Clearance />} />
+            </Routes>
         </div>
     );
 };
 
 export default MainRoutes;
-
-import React from 'react'
-import {Routes,Route} from "react-router-dom"
-import Clearance from './Clearance/Clearance'
-import Men from './Men/Men'
-import Women from './Women/Women'
-
-
-const MainRoutes = () => {
-  return (
-    <Routes>
-      <Route path="/women" element={<Women />} />
-      <Route path="/men" element={<Men />} />
-      <Route path="/clearance" element={<Clearance />} />
-    </Routes>
-  )
-}
-
-export default MainRoutes
-
