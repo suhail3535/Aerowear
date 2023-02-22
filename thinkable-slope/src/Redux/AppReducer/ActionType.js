e8721e6d681aee4b1df610233e25092e6be3b5f2
suhail3535 <khansohail015@gmail.com>
2023-02-22 12:33:48 +0530

    update store and action

---
+++ thinkable-slope/src/Redux/AppReducer/ActionType.js
@@ -0,0 +1,3 @@
+
+
+export const GET_REQUEST = "GET_REQUEST";
---
+++ thinkable-slope/src/Redux/AppReducer/reducer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const reducer = () => {
+  return (
+    <div>reducer</div>
+  )
+}
+
+export default reducer
