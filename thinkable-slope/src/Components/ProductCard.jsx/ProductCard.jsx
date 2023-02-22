d78dc3755e44e8ed9b2bf36bb9f71c551cdbd27e
suhail3535 <khansohail015@gmail.com>
2023-02-22 23:16:51 +0530

    dataupdate

--- thinkable-slope/src/Components/ProductCard.jsx/ProductCard.jsx
+++ thinkable-slope/src/Components/ProductCard.jsx/ProductCard.jsx
@@ -1,19 +1,19 @@
 import BasicFn from "../Stars"
 import styles from "./ProductCard.module.css"
 
-const ProductCard=({Image,name,Price,Rating,id})=>{
+const ProductCard=({image,name,price,Rating,id})=>{
 return(
     <>
     <div>   
      <p>{id}</p>
-    <img  src={Image} alt="" />
+    <img  src={image} alt="" />
     <div className={styles.limited_Edition_div}>
         <p>Limited Edition</p>
         <img  className={styles.cart_image}  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxi_iyZY7EC6BEEFo5YZtP0rdOoxp4PRtqhQ&usqp=CAU" alt="" />
     </div>
      <p className={styles.productCard_name}>{name}</p>
      <div className={styles.ProductCard_price_maindiv}>
-     <p className={styles.productCard_price}>$ {Price} <span>Comp. Value: $59.95</span> </p>
+     <p className={styles.productCard_price}>$ {price} <span>Comp. Value: $59.95</span> </p>
      </div>
      <BasicFn Rating={Rating}/>
     </div>
