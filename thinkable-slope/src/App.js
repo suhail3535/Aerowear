8fc7c5d7ec659f20c00ac6b183af03d9207f9230
Vivek Maurya <vivekmauryarkt1998@gmail.com>
2023-02-22 20:45:17 +0530

    just startedday-2

--- thinkable-slope/src/App.js
+++ thinkable-slope/src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Post from './Components/Admin/Post';
+import Cart from './Components/Cart/Cart';
 import Navbar from './Components/Navbar/Navbar';
 import MainRoutes from './Pages/MainRoutes';
 
@@ -8,8 +9,9 @@ function App() {
   return (
     <div className="App">
       {/*<Post />*/}
-      <Navbar/>
-      <MainRoutes/>
+      {/* <Navbar/>
+      <MainRoutes/> */}
+      <Cart/>
     </div>
   );
 }
---
+++ thinkable-slope/src/Components/Cart/Cart.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Card, CardBody, CardFooter, Image, Stack, Heading, Text, Button } from '@chakra-ui/react'
+const Cart = () => {
+  return (
+    <div>
+        <Card
+  direction={{ base: 'column', sm: 'row' }}
+  overflow='hidden'
+  variant='outline'
+>
+  <Image
+    objectFit='cover'
+    maxW={{ base: '100%', sm: '200px' }}
+    src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
+    alt='Caffe Latte'
+  />
+
+  <Stack>
+    <CardBody>
+      <Heading size='md'>The perfect latte</Heading>
+
+      <Text py='2'>
+        Caffè latte is a coffee beverage of Italian origin made with espresso
+        and steamed milk.
+      </Text>
+    </CardBody>
+
+    <CardFooter>
+      <Button variant='solid' colorScheme='blue'>
+        Buy Latte
+      </Button>
+    </CardFooter>
+  </Stack>
+</Card>
+    </div>
+  )
+}
+export default Cart
