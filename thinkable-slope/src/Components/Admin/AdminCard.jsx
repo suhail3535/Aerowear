a9eeda88db8c8794833206694844e5a67c9eaf04
suhail3535 <khansohail015@gmail.com>
2023-02-25 20:13:11 +0530

    day5 update payment page

--- thinkable-slope/src/Components/Admin/AdminCard.jsx
+++ thinkable-slope/src/Components/Admin/AdminCard.jsx
@@ -3,24 +3,55 @@ import styles from "./admin.module.css"
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import { deletedata, getRequest } from '../../Redux/AdminReducer/action';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Swal from "sweetalert2";
 const AdminCard = ({ id, image, name, price, Rating }) => {
     const dispatch = useDispatch()
-    
+    const navigate=useNavigate()
 
     const handleDelete = (id) => {
-        // console.log(id)
-        dispatch(deletedata(id)).then(() =>
-            dispatch(getRequest()));
-        // window.location.reload(true);
-  Swal.fire("", "Product deleted !", "success");
-
+        // console.log(id) 
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire("Deleted!", "Your file has been deleted.", "success");
+    }
+  });
+ dispatch(deletedata(id)).then(() => dispatch(getRequest()));
 }
 
-//     useEffect(() => {
-//     dispatch(getRequest());
-// },[])
+  const handleClick = () => {
+ let timerInterval;
+ Swal.fire({
+   title: "Admin please wait!",
+   html: "I will close in <b></b> milliseconds.",
+   timer: 1000,
+   timerProgressBar: true,
+   didOpen: () => {
+     Swal.showLoading();
+     const b = Swal.getHtmlContainer().querySelector("b");
+     timerInterval = setInterval(() => {
+       b.textContent = Swal.getTimerLeft();
+     }, 100);
+   },
+   willClose: () => {
+     clearInterval(timerInterval);
+   },
+ }).then((result) => {
+   /* Read more about handling dismissals below */
+   if (result.dismiss === Swal.DismissReason.timer) {
+     console.log("I was closed by the timer");
+   }
+ });
+
+}
 
   return (
     <div id={styles.card}>
@@ -42,16 +73,19 @@ const AdminCard = ({ id, image, name, price, Rating }) => {
         <p>Rating:{Rating}</p>
         <div id={styles.button}>
           <Link to={`/edit/${id}`}>
-            <Button style={{ width: "60px" }} colorScheme="cyan">
+            <Button
+              id={styles.edit}
+              onClick={handleClick}
+              style={{ width: "60px" }}
+            >
               Edit
             </Button>
           </Link>
           <Button
+            id={styles.delete}
             onClick={() => handleDelete(id)}
             style={{ width: "60px" }}
-            colorScheme="red"
           >
-            
             Delete
           </Button>
         </div>
